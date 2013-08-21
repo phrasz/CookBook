bee30fca496d0a61e37ae7c340f594a9d570a00f
phrasz <phrasz@github.com>
2013-08-20 21:43:38 -0400

    Added Allegro Timer

--- Allegro/SimpleTimer/main.c
+++ Allegro/SimpleTimer/main.c
@@ -7,8 +7,10 @@ int main(){
 	int redraw=0;
 	int tick_counter=0;
 	int seconds_counter=0;
+
 	float seconds = 1.0;
 	float FPS = 500.0;
+
 	ALLEGRO_EVENT_QUEUE *queue;						
 	ALLEGRO_TIMER *timer;
 	ALLEGRO_EVENT event;
@@ -22,52 +24,23 @@ int main(){
 	timer = al_create_timer(seconds / FPS);
 	al_start_timer(timer);
 	
-	//al_install_mouse();								
-	al_install_keyboard();
-	al_init_image_addon();
-	
-	al_register_event_source(queue, al_get_keyboard_event_source());
 	al_register_event_source(queue, al_get_timer_event_source(timer));
 	
 	while(Run_Magic_Loop){
 		al_wait_for_event(queue, &event);
 
-		if (event.type == ALLEGRO_EVENT_DISPLAY_CLOSE) 
-			break;
-		if (event.type == ALLEGRO_EVENT_KEY_CHAR){
-			switch(event.keyboard.keycode){
-				case ALLEGRO_KEY_ESCAPE:
-					printf("\nESC Key pressed!");
-					Run_Magic_Loop=0;
-					break;
-				default:
-					// Nothing
-					break;
-			}
-		}
-
-		if(event.type == ALLEGRO_EVENT_KEY_UP){
-			switch(event.keyboard.keycode){
-				//case ALLEGRO_KEY_ENTER:
-				//	break;
-				default:
-					// Nothing
-					break;
-			}
-		}
 		if(event.type == ALLEGRO_EVENT_TIMER){
 			redraw = true;
 		}
 		
 		if (redraw && al_is_event_queue_empty(queue)){	
 			redraw = false;
-			printf("\n IN Timer Loop!");
-			printf("\n\ttick_counter:%i , seconds_counter:%i",tick_counter,seconds_counter);
+			printf("\ntick_counter:%i, seconds_counter:%i",tick_counter,seconds_counter);
 			tick_counter++;
 			if(tick_counter%(int)FPS==0)
 				seconds_counter++;
 		}
 	}
-	return 0; //Program Completed Successfully
+	return 0;
 }
 
